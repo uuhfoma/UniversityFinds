598158b7d7477a85ddd21a45f772c2f09fd7ad97
Gilbert Ijoma <gijoma1@students.towson.edu>
2023-11-17 15:44:37 -0500

    User authentication and cookies added

--- nodejs/src/data_models/User/user-db.ts
+++ nodejs/src/data_models/User/user-db.ts
@@ -1,14 +1,144 @@
-import { DeleteResult, ObjectId, UpdateResult, Document } from 'mongodb'
+import { DeleteResult, ObjectId, UpdateResult, Document, InferIdType } from 'mongodb'
 import client from 'mongodb/client'
 import { User } from './user-model'
+import { webcrypto } from 'crypto'
 const db = client.db()
 
-export async function addUser(user: User): Promise<string |ObjectId> {
-	const users = db.collection<User>('users')
-	const { insertedId } = await users.insertOne({ ...user })
-	return insertedId
+export async function generateAuthToken() {
+	let tokenUnique = false
+	const characters =
+		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_'
+	let result = ''
+
+	while (!tokenUnique) {
+		result = ''
+		for (let i = 0; i < 10; i++) {
+			result += characters.charAt(
+				Math.floor(Math.random() * characters.length)
+			)
+		}
+		const AuthCollection = db.collection('userAuthentication')
+		const checkAuthExists = await AuthCollection.findOne({ token: result })
+		if (checkAuthExists == null) {
+			tokenUnique = true
+		}
+	}
+
+	return result
 }
 
+export async function createUser(userData: User) {
+	try {
+		console.log(userData)
+
+		userData.password = await hash(userData.password)
+		
+		
+		const usersCollection = db.collection<User>('users')
+		const result = await usersCollection.insertOne(userData)
+		const authToken = await generateAuthToken()
+		const AuthCollection = db.collection('userAuthentication')
+		await AuthCollection.insertOne({
+			user: result.insertedId,
+			token: authToken,
+		})
+
+		return { id: result.insertedId, token: authToken }
+	} catch (e) {
+		console.log(e)
+		return null
+	}
+}
+
+//LOGIN TO ACCOUNT
+export async function userLogin(userLogin: User) {
+	try {
+		const usersCollection = db.collection<User>('users')
+		const email = userLogin.studentEmail
+		const password = await hash(userLogin.password)
+		console.log('PASSWORD: ' + password)
+		const result = await usersCollection.findOne({
+			studentEmail: email,
+			password: password,
+		})
+
+		if (result == null) {
+			console.log('no account found')
+			return 'xxx'
+		} else {
+			console.log('ACCOUNT EXISTS')
+			const authToken = await generateAuthToken()
+			const AuthCollection = db.collection('userAuthentication')
+			await removeAuthTokens(result._id)
+			await AuthCollection.insertOne({
+				user: result._id,
+				token: authToken,
+			})
+			return authToken
+		}
+	} catch (e) {
+		console.log(e)
+		return null
+	}
+}
+
+async function hash(password: string) {
+	const utf8 = new TextEncoder().encode(password)
+	const hashBuffer = await webcrypto.subtle.digest('SHA-256', utf8)
+	const hashArray = Array.from(new Uint8Array(hashBuffer))
+	const hashHex = hashArray
+		.map((bytes) => bytes.toString(16).padStart(2, '0'))
+		.join('')
+
+	return hashHex
+}
+//LOGOUT OF ACCOUNT
+export async function userLogout(authToken: string) {
+	try {
+		const AuthCollection = db.collection('userAuthentication')
+		return await AuthCollection.deleteOne({ token: authToken })
+	} catch (e) {
+		console.log(e)
+		return null
+	}
+}
+
+//REMOVE EXISTING USER TOKEN
+export async function removeAuthTokens(userID: InferIdType<User>) {
+	try {
+		const AuthCollection = db.collection('userAuthentication')
+		return await AuthCollection.deleteOne({ user: userID })
+	} catch (e) {
+		console.log(e)
+		return null
+	}
+}
+
+//query database to get an account by email, returns if is found
+export async function getUserByEmail(email: string) {
+	try {
+		const usersCollection = db.collection<User>('users')
+		const result = await usersCollection.findOne({ studentEmail: email })
+		if (result == null) {
+			console.log('no email found')
+			return false
+		} else {
+			console.log('EMAIL EXISTS')
+			return true
+		}
+	} catch (e) {
+		console.log(e)
+		return null
+	}
+}
+
+// export async function addUser(user: User): Promise<string |ObjectId> {
+// 	const users = db.collection<User>('users')
+// 	const { insertedId } = await users.insertOne({ ...user })
+// 	return insertedId
+// }
+
+//used for profile card stack
 export async function getUsersExcept(ids: Array<ObjectId | string>) {
     const usersCollection = db.collection<User>('users');
   
--- nodejs/src/data_models/User/user-router.ts
+++ nodejs/src/data_models/User/user-router.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express'
 import * as userDB from './user-db'
 import { User } from './user-model'
-
+import {
+	createUser,
+	generateAuthToken,
+	getUserByEmail,
+	userLogin,
+	userLogout,
+} from './user-db'
+//Name of cookie
+const COOKIE_NAME = 'auth'
 const router = Router()
 
 router.get('/:id', async (req, res) => {
@@ -26,6 +34,61 @@ router.get('/:id', async (req, res) => {
 		})
 })
 
+//LOGIN TO ACCOUNT
+router.post('/login', async (req, res) => {
+	userLogin(req.body).then(function (result) {
+		console.log('sending confirmation: ' + result)
+		if (result == 'xxx') {
+			res.sendStatus(401)
+		} else {
+			res.cookie(COOKIE_NAME, result, {
+				maxAge: 1000 * 60 * 60 * 24, //24 hours
+				secure: true,
+			}).sendStatus(201)
+		}
+	})
+})
+
+//LOGOUT OF ACCOUNT
+router.post('/logout', async (req, res) => {
+	//console.log(req.body)
+	const authToken = req.body.authToken
+	userLogout(authToken).then(function (result) {
+		console.log('sending confirmation: ' + result)
+		if (!result) {
+			res.sendStatus(401)
+		} else {
+			res.clearCookie(COOKIE_NAME).sendStatus(201)
+		}
+	})
+})
+
+//Create User
+router.post('/create', async (req, res) => {
+	const user: User = req.body
+
+	let result = await createUser(req.body)
+
+	if (!result) {
+		res.send(400)
+		return
+	}
+	user._id = result.id
+
+	res.cookie(COOKIE_NAME, result.token, {
+		maxAge: 1000 * 60 * 60 * 24, //24 hours
+		secure: true,
+	}).json(user)
+})
+
+//check if emailexists
+router.post('/accountByEmail', async (req, res) => {
+	console.log('email TO SEND: ' + req.body.studentEmail)
+	getUserByEmail(req.body.studentEmail).then((getEmail) => {
+		console.log('Email exists? ' + getEmail)
+		res.json({ getEmail })
+	})
+})
 //gets all users except the specified id
 router.get('/profilestack/:id', async (req, res) => {
     const { id } = req.params
@@ -61,23 +124,23 @@ router.put('/update/:id', async (req, res) => {
 		})
 })
 
-router.post('/add', async (req, res) => {
-	const user: User = req.body
+// router.post('/add', async (req, res) => {
+// 	const user: User = req.body
 
-	console.log('Adding user: ', user)
+// 	console.log('Adding user: ', user)
 
-	userDB
-		.addUser(user)
-		.then((id) => {
-			console.log('Added user with id:', id)
-			res.json({ id: id })
-		})
-		.catch((err) => {
-			console.log(err)
+// 	userDB
+// 		.addUser(user)
+// 		.then((id) => {
+// 			console.log('Added user with id:', id)
+// 			res.json({ id: id })
+// 		})
+// 		.catch((err) => {
+// 			console.log(err)
 
-			res.sendStatus(500)
-		})
-})
+// 			res.sendStatus(500)
+// 		})
+// })
 
 router.delete('/delete/:id', async (req, res) => {
 	const { id } = req.params
