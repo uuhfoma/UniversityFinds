cd034e456526f6774bc3eebf563e7ecd91e73acf
uuhfoma <ritafoma50@gmail.com>
2023-11-30 18:29:32 -0500

    Main. page work

--- react-app/src/views/home/Home.tsx
+++ react-app/src/views/home/Home.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
-import styles from '/Home.module.css'
-//  import video from '../src/layouts/public/Home.videoTU.mp4'
+import styles from'./Home.module.css'
 
 const Home: React.FC = () => {
   return (
     <div className="Home">
-      <h1>Welcome to the home page</h1>
-    <ReactPlayer url={" /src/layouts/public/Home.videoTU.mp4"} playing 
-      muted
-      loop
-      width="100%"
-      height="100vh"
+    <ReactPlayer className={styles.reactPlayer}
+    url={"/src/layouts/public/Home.videoTU.mp4"} 
+     width="100%"
+     height="100%"
+     playing  
+     muted 
+     loop
+     
       />
   </div>
   );
