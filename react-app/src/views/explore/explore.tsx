f8485924639249309e8b77a6dd4a1d84ccc66388
Muna D. Okeke <munaokeke02@gmail.com>
2023-12-01 15:53:06 -0500

    Added to intro explore page

--- react-app/src/views/explore/explore.tsx
+++ react-app/src/views/explore/explore.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Explore: React.FC = () => {
-  return (
-    <div>
+ return (
+    <div className="centered-container">
       {/* Component content goes here */}
+      <h1>Unlock a World of Connections: Welcome to UniversityFinds – Where Friendships Know No Borders!</h1>
     </div>
-  );
+ );
 };
 
 export default Explore;
