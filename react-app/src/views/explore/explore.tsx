60739d5769f8a4ea2d6e618ea18c38c82dba6b88
Muna D. Okeke <munaokeke02@gmail.com>
2023-12-01 21:33:27 -0500

    TRied video

--- react-app/src/views/explore/explore.tsx
+++ react-app/src/views/explore/explore.tsx
@@ -1,20 +1,87 @@
-import React from 'react';
-import UserFind from './UserFind';
+import React, { useState } from 'react';
+import './usersfind.css';
 
-const Explore: React.FC = () => {
-    const onSearch = (value: string) => {
-        console.log('Searching for:', value);
-    };
+interface User {
+  name: string;
+  age: number;
+  major: string;
+  university?: string;
+  gender?: string;
+}
 
-    return (
-        <div className="centered-container">
-            {/* Component content goes here */}
-      
-            {/* Added UserFind component */}
-            <UserFind 
-            />
-        </div>
-    );
+const UserFind: React.FC = () => {
+  const users: User[] = [
+    {
+      name: 'Syket',
+      age: 20,
+      university: 'Towson University',
+      major: 'Computer Science',
+      gender: 'Male',
+    },
+    {
+      name: 'Sakib',
+      age: 25,
+      major: 'Programmer',
+      gender: 'Male',
+    },
+    {
+      name: 'Jamy',
+      age: 30,
+      major: 'Designer',
+      gender: 'Female',
+    },
+    {
+      name: 'Hanif',
+      age: 20,
+      major: 'UX Writer',
+      gender: 'Male',
+    },
+  ];
+
+  const [userList, setUserList] = useState<User[] | undefined>(users);
+  const [text, setText] = useState<string>('');
+
+  const handleOnClick = () => {
+    const findUniversity = userList?.filter((u) => u.name === text);
+    setUserList(findUniversity);
+  };
+
+  return (
+    <div>
+      <div className="title">
+        <h1>Unlock a World of Connections: Welcome to UniversityFinds – Where Friendships Know No Borders!</h1>
+      </div>
+      <div className="input__wrapper">
+        <input
+          type="text"
+          placeholder="Search University"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button disabled={!text} onClick={handleOnClick}>
+          Search
+        </button>
+      </div>
+
+      <div className="body">
+        {userList && userList.length === 0 && (
+          <div className="notFound">No University Found</div>
+        )}
+
+        {userList &&
+          userList.length > 0 &&
+          userList.map((user) => (
+            <div className="body__item" key={user.name}>
+              <h3>Name: {user.name}</h3>
+              <p>Age: {user.age}</p>
+              <p>Major: {user.major}</p>
+              {user.university && <p>University: {user.university}</p>}
+              {user.gender && <p>Gender: {user.gender}</p>}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
-export default Explore;
+export default UserFind;
